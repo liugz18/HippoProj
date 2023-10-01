63a76e71eb962143cb1cf9add99309ef4270d1bc
Blake <liugztat@gmail.com>
2023-10-01 00:46:25 +0000

    add readme

--- model/ait_vitmatte.py
+++ model/ait_vitmatte.py
@@ -137,34 +137,34 @@ class AITVitMatteBasicConv3x3(nn.Module):
         return hidden_state
 
 
-# class AITVitMatteConvStream(tnn.Module):
-#     """
-#     Simple ConvStream containing a series of basic conv3x3 layers to extract detail features.
-#     """
+class AITVitMatteConvStream(tnn.Module):
+    """
+    Simple ConvStream containing a series of basic conv3x3 layers to extract detail features.
+    """
 
-#     def __init__(self, config):
-#         super().__init__()
+    def __init__(self, config):
+        super().__init__()
 
-#         in_channels = config.backbone_config.num_channels
-#         out_channels = config.convstream_hidden_sizes
+        in_channels = config.backbone_config.num_channels
+        out_channels = config.convstream_hidden_sizes
 
-#         self.convs = tnn.ModuleList()
-#         self.conv_chans = [in_channels] + out_channels
+        self.convs = tnn.ModuleList()
+        self.conv_chans = [in_channels] + out_channels
 
-#         for i in range(len(self.conv_chans) - 1):
-#             in_chan_ = self.conv_chans[i]
-#             out_chan_ = self.conv_chans[i + 1]
-#             self.convs.append(AITVitMatteBasicConv3x3(config, in_chan_, out_chan_))
+        for i in range(len(self.conv_chans) - 1):
+            in_chan_ = self.conv_chans[i]
+            out_chan_ = self.conv_chans[i + 1]
+            self.convs.append(AITVitMatteBasicConv3x3(config, in_chan_, out_chan_))
 
-#     def forward(self, pixel_values):
-#         out_dict = {"detailed_feature_map_0": pixel_values}
-#         embeddings = pixel_values
-#         for i in range(len(self.convs)):
-#             embeddings = self.convs[i](embeddings)
-#             name_ = "detailed_feature_map_" + str(i + 1)
-#             out_dict[name_] = embeddings
+    def forward(self, pixel_values):
+        out_dict = {"detailed_feature_map_0": pixel_values}
+        embeddings = pixel_values
+        for i in range(len(self.convs)):
+            embeddings = self.convs[i](embeddings)
+            name_ = "detailed_feature_map_" + str(i + 1)
+            out_dict[name_] = embeddings
 
-#         return out_dict
+        return out_dict
 
 
 # class AITVitMatteFusionBlock(tnn.Module):
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Blake Mini-Project on AIT
+
+## Run module unit tests 
+
+```bash
+python -m tests.test_VitDetMlp
+
+python -m tests.test_VitDetLayerNorm
+
+python -m tests.test_VitDetResBottleneckBlock
+
+python -m tests.test_VitMatteBasicConv3x3
+```
